4c561c6c8a92ff3451398d98d5acdd6d6960ecfe
Joseph I Jess <12313215+Joseph-I-Jess@users.noreply.github.com>
2024-05-15 11:25:39 -0700

    Now with class, selfish, selfish class!

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 import tkinter
 
-def button_clicked(label, entry):
-    label.config(text=entry.get())
+class GameGui:
+
+    def __init__(self):
+        root = tkinter.Tk()
+
+        label = tkinter.Label(master=root, text="this is a label")
+        entry = tkinter.Entry(master=root)
+        button = tkinter.Button(master= root, text="click the button!", command=lambda: self.button_clicked(label, entry))
+
+        label.pack()
+        entry.pack()
+        button.pack()
+    
+        root.mainloop()
+
+    def button_clicked(self, label, entry):
+        label.config(text=entry.get())
 
 def main():
-    root = tkinter.Tk()
+    my_game = GameGui()
 
-    label = tkinter.Label(master=root, text="this is a label")
-    entry = tkinter.Entry(master=root)
-    button = tkinter.Button(master= root, text="click the button!", command=lambda: button_clicked(label, entry))
-
-    label.pack()
-    entry.pack()
-    button.pack()
-
-    root.mainloop()
 
 if __name__ == "__main__":
     main()
