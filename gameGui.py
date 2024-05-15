fc2099b265b425ba553be7a6f547473203e33637
Joseph I Jess <12313215+Joseph-I-Jess@users.noreply.github.com>
2024-05-15 11:56:15 -0700

    now with right movement detected and entry focus by default!

--- gameGui.py
+++ gameGui.py
@@ -10,14 +10,20 @@ class GameGui:
         self.entry.bind("<Return>", self.entry_entered)
         self.canvas = tkinter.Canvas(master=self.root, width=600, height=400, background="darkgray")
 
-        self.canvas.create_rectangle(20, 20, 40, 60, fill="green")
+        self.player = self.canvas.create_rectangle(20, 20, 40, 60, fill="green") # canvas created objects start at index 1
 
         self.canvas.pack()
         self.label.pack()
         self.entry.pack()
 
+        self.entry.focus()
+
     def entry_entered(self, event):
-        self.label.config(text=self.entry.get())
+        input = self.entry.get()
+
+        if input == "right":
+            self.canvas.move(self.player, 10, 0) # move x and y distance of a given index in canvas
+            
 
     def run(self):
         self.root.mainloop()
