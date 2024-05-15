78fda14d7bb1df51ba51329aa33461ac6af47294
Joseph I Jess <12313215+Joseph-I-Jess@users.noreply.github.com>
2024-05-15 11:22:13 -0700

    now with a command!

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import tkinter
 
+def button_clicked(label, entry):
+    label.config(text=entry.get())
+
 def main():
     root = tkinter.Tk()
 
     label = tkinter.Label(master=root, text="this is a label")
     entry = tkinter.Entry(master=root)
-    button = tkinter.Button(master= root, text="click the button!")
+    button = tkinter.Button(master= root, text="click the button!", command=lambda: button_clicked(label, entry))
 
     label.pack()
     entry.pack()
