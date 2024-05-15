3d8b9b861313dfffeea434ef91c31960c945ca51
Joseph I Jess <12313215+Joseph-I-Jess@users.noreply.github.com>
2024-05-15 11:50:07 -0700

    now with a canvas and rectangle!

--- gameGui.py
+++ gameGui.py
@@ -5,10 +5,14 @@ class GameGui:
     def __init__(self):
         self.root = tkinter.Tk()
 
-        self.label = tkinter.Label(master=self.root, text="this is a label")
+        self.label = tkinter.Label(master=self.root, text="type a dcommand and press return (enter)")
         self.entry = tkinter.Entry(master=self.root)
         self.entry.bind("<Return>", self.entry_entered)
+        self.canvas = tkinter.Canvas(master=self.root, width=600, height=400, background="darkgray")
 
+        self.canvas.create_rectangle(20, 20, 40, 60, fill="green")
+
+        self.canvas.pack()
         self.label.pack()
         self.entry.pack()
 
