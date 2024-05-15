4832ae964ac06e1ef93f222c2b0b6b3973fbedc5
Joseph I Jess <12313215+Joseph-I-Jess@users.noreply.github.com>
2024-05-15 11:19:44 -0700

    now a basic GUI

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import tkinter
 
 def main():
-    print("hello?")
+    root = tkinter.Tk()
+
+    label = tkinter.Label(master=root, text="this is a label")
+    entry = tkinter.Entry(master=root)
+    button = tkinter.Button(master= root, text="click the button!")
+
+    label.pack()
+    entry.pack()
+    button.pack()
+
+    root.mainloop()
 
 if __name__ == "__main__":
     main()
